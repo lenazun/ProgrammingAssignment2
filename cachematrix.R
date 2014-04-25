ef57772640b9a7e914ddce90a003cde013518d14
Lena <lenazun@gmail.com>
2014-04-25 11:27:38 -0700

    2n assignment with comments and corrections

--- cachematrix.R
+++ cachematrix.R
@@ -3,25 +3,31 @@
 
 makeCacheMatrix <- function(x = matrix()) {
   
+  #set the value of the cache matrix to null
   cachematrix <- NULL
   
+  #function to set the value of the matrix
   set <- function(mtrx) {
     x <<- mtrx
     cachematrix <<- NULL
   }
   
+  #function to get the matrix data
   get <- function() {
     x
   }
   
+  #set the matrix and cache 
   setmatrix <- function(pmatrix) {
     cachematrix <<- pmatrix
   }
   
+  #get the value of the matrix
   getmatrix <- function() { 
-    cachematrix> a <- makeCacheMatrix(matrix(1:4,2))
+    cachematrix
   }
   
+  #organize everything as a list
   list(set = set, get = get,
        setmatrix = setmatrix,
        getmatrix = getmatrix)
@@ -29,15 +35,20 @@ makeCacheMatrix <- function(x = matrix()) {
 
   
 
-## Write a short comment describing this function
-## Return a matrix that is the inverse of 'x'
+## This function returns the value of the inverse of the matrix
 
 cacheSolve <- function(x, ...) {
+
+#gets the value of the matrix  
   m <- x$getmatrix()
+  
+#if there is a cache retrieves the value  
   if(!is.null(m)) {
     message("getting cached data")
     return(m)
   }
+
+#returns the reverse of the matrix
   data <- x$get()
   m <- solve(data, ...)
   x$setmatrix(m)
